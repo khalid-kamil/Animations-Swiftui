3badb4d495f834eb41b564377ae17070b5ebb509
Khalid Kamil <carts_carps_0o@icloud.com>
2022-01-21 20:01:38 +0000

    Initial Commit

---
+++ Shared/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Shared
+//
+//  Created by Khalid Kamil on 21/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
